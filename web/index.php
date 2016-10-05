9a12d5fbe6470e6c1e5364623774424e699f6b00
Tyron Sale <150068@virtualwindow.co.za>
2016-10-05 18:46:01 +0200

    Testing commit from PHPStorm

--- web/index.php
+++ web/index.php
@@ -2,7 +2,6 @@
     //AUTOLOAD
     //TESTING PHPstorm
     require_once __DIR__.'/../vendor/autoload.php';
-    require_once __DIR__.'/../vendor/autoload.php';
 
     use Symfony\Component\HttpFoundation\Request;
     use Symfony\Component\HttpFoundation\Response;
