1f9170a824ab29b70f1a8f3d2184a6a2c1f766d8
Duan Viljoen <duanviljoen@Duan-Viljoens-MacBook-Pro.local>
2016-10-05 10:02:48 +0200

    Adding/Refactoring for Silex/Twig

---
+++ web/index.php
@@ -0,0 +1,24 @@
+<?php
+    //AUTOLOAD
+    require_once __DIR__.'/../vendor/autoload.php';
+    
+    use Symfony\Component\HttpFoundation\Request;
+    use Symfony\Component\HttpFoundation\Response;
+
+    $app = new Silex\Application();
+    
+    //SERVICE PROVIDER
+    $app->register(new Silex\Provider\SessionServiceProvider());
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/views',
+    ));
+
+    //ROUTES
+    $app->get('/', function(Request $request) use ($app) {
+        return $app['twig']->render('home.twig', array());
+    });
+    
+    //RUN APP
+    $app->run();
+
+?>
---
+++ web/views/layout.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="/magnify/web/css/main.css">
+    <link rel="stylesheet" href="/magnify/web/css/home.css">
+    <link href="https://fonts.googleapis.com/css?family=Ledger" rel="stylesheet">
+</head>
+<body>
+    {% block content %}
+    
+    {% endblock %}
+</body>
+</html>
