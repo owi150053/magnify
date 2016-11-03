28c01f6e326fd6aaaa824d1b2199e1d2a0b94f77
Duan Viljoen <Duan Viljoen>
2016-11-04 01:34:01 +0200

    LIKING SYSTEM

--- web/delegates/post_delegate.php
+++ web/delegates/post_delegate.php
@@ -4,7 +4,7 @@
 
         $sql = "SELECT posts.id, posts.title, posts.content, posts.image_path, posts.author_id, posts.post_date, users.name, users.surname, users.avatar_path, users.id as user_id FROM posts
         INNER JOIN users
-        ON author_id=users.id;";
+        ON author_id=users.id";
         $statement = $pdo->prepare($sql);
         $statement->execute();
         $results = $statement->fetchAll();
